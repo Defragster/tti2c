01967ae9532f536550162c0edd83b9ac7d3ada86
Kurt Eckhardt <kurte@rockisland.com>
2020-01-25 13:47:10 -0800

    Create GetDistanceI2c_ssd1306.ino
    Reads the Lidar lite on Wire
    and shows the disance on SSD1306 on Wire1.
    
    May extend some.

---
+++ examples/GetDistanceI2c_ssd1306/GetDistanceI2c_ssd1306.ino
@@ -0,0 +1,123 @@
+/*------------------------------------------------------------------------------
+
+  LIDARLite Arduino Library
+  v3/GetDistanceI2c
+
+  This example shows how to initialize, configure, and read distance from a
+  LIDAR-Lite connected over the I2C interface.
+
+  Connections:
+  LIDAR-Lite 5 Vdc (red) to Arduino 5v
+  LIDAR-Lite I2C SCL (green) to Arduino SCL
+  LIDAR-Lite I2C SDA (blue) to Arduino SDA
+  LIDAR-Lite Ground (black) to Arduino GND
+
+  (Capacitor recommended to mitigate inrush current when device is enabled)
+  680uF capacitor (+) to Arduino 5v
+  680uF capacitor (-) to Arduino GND
+
+  See the Operation Manual for wiring diagrams and more information:
+  http://static.garmin.com/pumac/LIDAR_Lite_v3_Operation_Manual_and_Technical_Specifications.pdf
+
+  ------------------------------------------------------------------------------*/
+
+#include <Wire.h>
+#include <LIDARLite.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, OLED_RESET);
+
+LIDARLite myLidarLite;
+
+void setup()
+{
+  Serial.begin(115200); // Initialize serial connection to display distance readings
+
+  /*
+    begin(int configuration, bool fasti2c, char lidarliteAddress)
+
+    Starts the sensor and I2C.
+
+    Parameters
+    ----------------------------------------------------------------------------
+    configuration: Default 0. Selects one of several preset configurations.
+    fasti2c: Default 100 kHz. I2C base frequency.
+      If true I2C frequency is set to 400kHz.
+    lidarliteAddress: Default 0x62. Fill in new address here if changed. See
+      operating manual for instructions.
+  */
+  myLidarLite.begin(0, true); // Set configuration to default and I2C to 400 kHz
+
+  /*
+    configure(int configuration, char lidarliteAddress)
+
+    Selects one of several preset configurations.
+
+    Parameters
+    ----------------------------------------------------------------------------
+    configuration:  Default 0.
+      0: Default mode, balanced performance.
+      1: Short range, high speed. Uses 0x1d maximum acquisition count.
+      2: Default range, higher speed short range. Turns on quick termination
+          detection for faster measurements at short range (with decreased
+          accuracy)
+      3: Maximum range. Uses 0xff maximum acquisition count.
+      4: High sensitivity detection. Overrides default valid measurement detection
+          algorithm, and uses a threshold value for high sensitivity and noise.
+      5: Low sensitivity detection. Overrides default valid measurement detection
+          algorithm, and uses a threshold value for low sensitivity and noise.
+    lidarliteAddress: Default 0x62. Fill in new address here if changed. See
+      operating manual for instructions.
+  */
+  myLidarLite.configure(0); // Change this number to try out alternate configurations
+
+  // Now try to start the SSD1306 display
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println("SSD1306 failed to start\n");
+  }
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0, 0);            // Start at top-left corner
+  display.println(F("Hello"));
+  display.display();
+
+}
+
+uint16_t last_distance = 0xffff;
+uint8_t loop_count = 0;
+void loop()
+{
+  /*
+    distance(bool biasCorrection, char lidarliteAddress)
+
+    Take a distance measurement and read the result.
+
+    Parameters
+    ----------------------------------------------------------------------------
+    biasCorrection: Default true. Take aquisition with receiver bias
+      correction. If set to false measurements will be faster. Receiver bias
+      correction must be performed periodically. (e.g. 1 out of every 100
+      readings).
+    lidarliteAddress: Default 0x62. Fill in new address here if changed. See
+      operating manual for instructions.
+  */
+  uint16_t distance =  myLidarLite.distance(loop_count == 0);
+  if (distance != last_distance) {
+    last_distance = distance;
+    display.clearDisplay();
+    display.setCursor(0, 0);            // Start at top-left corner
+    display.print(distance, DEC);
+    display.display();
+    Serial.println(distance);
+  }
+  loop_count = (++loop_count) & 0x3f;
+}
